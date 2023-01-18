54d36053289c8f742c5d35a233b6d75bf018aafe
たことぱす <79318163+tacotopasu@users.noreply.github.com>
2023-01-18 13:02:01 +0000

    Small changes

--- src/main/java/net/ramencafe/forwardrooms/ForwardRooms.java
+++ src/main/java/net/ramencafe/forwardrooms/ForwardRooms.java
@@ -1,4 +1,4 @@
-package com.ramencafe.forwardrooms;
+package net.ramencafe.forwardrooms;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
@@ -19,9 +19,10 @@ import org.apache.logging.log4j.Logger;
 import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod("forwardrooms")
+@Mod(ForwardRooms.MOD_ID)
 public class ForwardRooms
 {
+    public static final String MOD_ID = "forwardrooms";
     // Directly reference a log4j logger.
     private static final Logger LOGGER = LogManager.getLogger();
 
@@ -48,13 +49,12 @@ public class ForwardRooms
 
     private void doClientStuff(final FMLClientSetupEvent event) {
         // do something that can only be done on the client
-        LOGGER.info("Got game settings {}", event.getMinecraftSupplier().get().options);
     }
 
     private void enqueueIMC(final InterModEnqueueEvent event)
     {
         // some example code to dispatch IMC to another mod
-        InterModComms.sendTo("examplemod", "helloworld", () -> { LOGGER.info("Hello world from the MDK"); return "Hello world";});
+        InterModComms.sendTo(MOD_ID, "helloworld", () -> { LOGGER.info("Hello world from the CCPhttps://www.amazon.com/gp/bestsellers/hi/511394"); return "Hello world";});
     }
 
     private void processIMC(final InterModProcessEvent event)
