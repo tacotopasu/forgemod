84402a39aa3b00bd793b1c7a7a4f99403e6cdbe2
AcridGuedes <ivogudes1907@gmail.com>
2023-01-26 22:59:25 +0000

    Added lamp that accualy emits light and added the 6 textures to wallpaper_1. So we can start builing the forwardrooms Much Love Ivo <3

--- src/main/java/net/ramencafe/forwardrooms/block/ModBlocks.java
+++ src/main/java/net/ramencafe/forwardrooms/block/ModBlocks.java
@@ -9,6 +9,7 @@ import net.minecraft.item.Item;
 import net.minecraftforge.common.ToolType;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.RegistryObject;
+import net.minecraftforge.fml.network.NetworkEvent;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.ramencafe.forwardrooms.ForwardRooms;
@@ -21,6 +22,7 @@ public class ModBlocks {
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, ForwardRooms.MOD_ID);
 
     public static final RegistryObject<Block> WALLPAPER_1 = registerBlock("wallpaper_1", () -> new Block(AbstractBlock.Properties.create(Material.ROCK).hardnessAndResistance(50)));
+    public static final RegistryObject<Block> LIGHTS_YES = registerBlock("light", () -> new Block(AbstractBlock.Properties.create(Material.ROCK).setLightLevel(value -> 10)));
 
     // Never use capital chars in item names!!!
     private static <T extends Block>RegistryObject<T> registerBlock(String name, Supplier<T> block){
