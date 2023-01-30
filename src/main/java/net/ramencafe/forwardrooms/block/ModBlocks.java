0728add99dc920229249823e0534d52c26ef3cce
AcridGuedes <ivogudes1907@gmail.com>
2023-01-30 18:08:21 +0000

    Renamed cealing.png to ceiling.png :)

--- src/main/java/net/ramencafe/forwardrooms/block/ModBlocks.java
+++ src/main/java/net/ramencafe/forwardrooms/block/ModBlocks.java
@@ -19,8 +19,10 @@ public class ModBlocks {
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, ForwardRooms.MOD_ID);
 
     public static final RegistryObject<Block> WALLPAPER = registerBlock("wallpaper", () -> new Block(AbstractBlock.Properties.create(Material.ROCK).hardnessAndResistance(50)));
+
     public static final RegistryObject<Block> LIGHT = registerBlock("light", () -> new Block(AbstractBlock.Properties.create(Material.ROCK)
             .setLightLevel(value -> 10)));
+
 
     // Never use capital chars in item names!!!
     private static <T extends Block>RegistryObject<T> registerBlock(String name, Supplier<T> block){
